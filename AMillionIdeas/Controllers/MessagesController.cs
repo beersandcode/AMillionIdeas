6b83688511e9576215cd4fc811919e18c124e014
rnieva <roberto.nieva@gmail.com>
2017-12-10 11:25:51 +0000

    Add Messages Controller Working in Messages-Chat-Users

---
+++ AMillionIdeas/Controllers/MessagesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AMillionIdeas.Controllers
+{
+    public class MessagesController : Controller
+    {
+
+        public ActionResult SendMessage(int id, int idDestUser) // user id registered, 
+        {
+            return View();
+        }
+
+        public ActionResult DeleteMessage(int id) 
+        {
+            return View();
+        }
+
+
+        public ActionResult ShowMessages(int id) 
+        {
+            // show all the messages, order by users or chats
+            return View();
+        }
+}
